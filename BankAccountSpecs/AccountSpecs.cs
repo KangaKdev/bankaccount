1707720cb29776e9d09ff2657ebf4f486494e80f
KangaKDEV <bla>
2016-12-02 10:45:47 +0100

    id, name und balance werden gesetzt + Tests

--- BankAccountSpecs/AccountSpecs.cs
+++ BankAccountSpecs/AccountSpecs.cs
@@ -13,14 +13,50 @@ namespace BankAccountSpecs
     public class AccountSpecs
     {
 
-        [TestCase(0)]
-        [TestCase(10000)]
+        [TestCase((ulong)0)]
+        [TestCase((ulong)10000)]
         public void A_new_account_is_initialized_with_correct_balance(ulong balance)
         {
-            Account account = new Account(balance);
-            int currentbalance = account.GetBalance();
+            uint id = 0;
+            string name = "hubbabubba";
+
+            Account account = new Account(id, name, balance);
+            ulong currentbalance = account.GetBalance();
             Assert.AreEqual(balance, currentbalance);
         }
+
+        [Test]
+        public void A_new_account_can_initialized_with_no_balance()
+        {
+            uint id = 0;
+            string name = "hubbabubba";
+            Account account = new Account(id, name);
+            ulong currentbalance = account.GetBalance();
+            Assert.AreEqual((ulong)0, currentbalance);
+        }
+
+        [Test]
+        public void When_A_account_is_created_the_name_is_stored()
+        {
+            uint id = 0;
+            string name = "hubbabubba";
+            Account account = new Account(id, name);
+            string accountname = account.GetName();
+            Assert.AreEqual(name, accountname);
+        }
+
+
+        [TestCase((uint)12345)]
+        [TestCase((uint)54321)]
+        public void When_A_account_is_created_the_id_is_stored(uint id)
+        {
+            string name = "hubbabubba";
+            Account account = new Account(id, name);
+            uint accountid = account.GetID();
+            Assert.AreEqual(id, accountid);
+        }
+
+
 
     }
 }
